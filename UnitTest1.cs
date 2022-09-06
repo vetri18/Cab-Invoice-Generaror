9b088233df7aa293e3e2ac1a8e2070c2b4dceb05
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-06 15:50:45 +0530

    unit test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,17 +1,31 @@
+using CabInvoiceGenerator;
+
 namespace CabInvoiceGeneratorUnitTest
 {
     [TestClass]
     public class UnitTest1
     {
-        
-        InvoiceGenerator invoiceGenerator = null
+        InvoiceGenerator invoiceGenerator = null;
         [TestMethod]
         public void TestMethod1()
         {
             invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             double distance = 2.0;
             int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+        [TestMethod]
+        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride ( 2.0, 5 ), new Ride ( 0.1, 2 ) };
 
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expected = new InvoiceSummary ( 2, 30.0 );
+
+            Assert.AreEqual(expected, invoiceSummary);
         }
     }
 }
